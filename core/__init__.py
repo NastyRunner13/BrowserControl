67dbcf3c03bb13f7998daa1a6a0d38fe5b9a9835
NastyRunner13 <princegupta1586@gmail.com>
2026-01-29 00:53:59 +0530

    chore: update exports and add new env settings to example - Export TabManager, TaskContext, DynamicAutomationAgent from core - Update .env.example with new security settings

--- core/__init__.py
+++ core/__init__.py
@@ -1,12 +1,17 @@
 from .browser_pool import BrowserPool, BrowserInstance
 from .element_finder import IntelligentElementFinder
 from .executor import IntelligentParallelExecutor
-from .planner import AutomationAgent  
+from .planner import AutomationAgent, DynamicAutomationAgent
+from .tab_manager import TabManager
+from .task_context import TaskContext
 
 __all__ = [
     'BrowserPool',
     'BrowserInstance',
     'IntelligentElementFinder',
     'IntelligentParallelExecutor',
-    'AutomationAgent' 
+    'AutomationAgent',
+    'DynamicAutomationAgent',
+    'TabManager',
+    'TaskContext'
 ]
